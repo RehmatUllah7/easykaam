91c0767fe72764abfca9ca469be093ad76db884a
matharehsan <muhammadatharehsan2@gamil.com>
2025-11-29 23:16:14 +0500

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <Header />
 
       <div className="p-10">
-        <h1 className="text-3xl text-red-100">Welcome to </h1>
+        <h1 className="text-3xl text-red-100">W </h1>
         <p className="mt-3 text-black-600">
      
         </p>
